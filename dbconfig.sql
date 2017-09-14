157878425bebf39fe89dbdc8ec59b5a00bbe6e1a
BerentM <berent.millo@gmail.com>
2017-09-14 23:50:12 +0200

    sql tworzacy tabele

---
+++ dbconfig.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE IF NOT EXISTS crimes (
+    id SERIAL NOT NULL PRIMARY KEY,
+    latitude FLOAT,
+    longitude FLOAT,
+    date date,
+    category VARCHAR(50),
+    description VARCHAR(1000),
+    updated_at TIMESTAMP);
+
+SELECT * FROM crimes;
