1c992692798e9561be826747eee87b66a7f9bba2
BerentM <berent.millo@gmail.com>
2017-09-15 20:21:46 +0200

    ogarnalem inserty

--- dbhelper.py
+++ dbhelper.py
@@ -23,10 +23,10 @@ class DBHelper():
     def add_input(self, data):
         connection = self.connect()
         query = "INSERT INTO crimes (description) VALUES ("+data+");"
-        with connection.cursor() as cursor:
-            cursor.execute(query)
+        cursor = connection.cursor()
+        cursor.execute(query)
         connection.commit()
-        connection.close()
+        # connection.close()
 
     def clear_all(self):
         connection = self.connect()
@@ -35,6 +35,6 @@ class DBHelper():
             with connection.cursor() as cursor:
                 cursor.execute(query)
             connection.commit()
-            connection.close()
+            # connection.close()
         except Exception as e:
             print(e)
